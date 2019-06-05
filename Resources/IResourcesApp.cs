e3c26581d08c2ca5c665abb5a27736de4bc72a94
ISPLab <for_orlov@mail.ru>
2019-06-05 09:53:55 +0300

    add resourses

---
+++ Resources/IResourcesApp.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P8.Services.Resourses
+{
+    public interface IResourcesApp
+    {
+        string GetString(string key);
+    }
+}
